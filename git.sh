1fc730584b82c862b5ed1260f0ca1ecc02b4ee41
ByUsi-link <14266085+byusi-link@user.noreply.gitee.com>
2025-02-18 21:39:47 +0800

    提交

--- git.sh
+++ git.sh
@@ -1,3 +1,5 @@
+git config --global user.name 'ByUsi-link' 
+git config --global user.email '14266085+byusi-link@user.noreply.gitee.com'
 git add .
 git commit -m "$1"
 git remote add origin https://gitee.com/byusi/proxy.git
