0e6b635795b102a97900f016cb6679be0b9096df
ByUsi-link <14266085+shibeiane@user.noreply.gitee.com>
2025-02-20 15:01:30 +0800

    移除不必要的sh脚本

--- git.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.name '北啊呢' 
-git config --global user.email '14266085+shibeiane@user.noreply.gitee.com'
-git add .
-git commit -m "$1"
-git remote add origin https://gitee.com/byusi/proxy.git
-git push -u origin "master"
