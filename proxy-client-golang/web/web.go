a7bdd701c907f264924ea6d25c116bbdb94a5333
ByUsi-link <177828525@qq.com>
2025-04-22 03:27:41 +0800

    客户端日志支持

--- proxy-client-golang/web/web.go
+++ proxy-client-golang/web/web.go
@@ -22,10 +22,69 @@ import (
 //go:embed *
 var staticFs embed.FS
 
-// 创建一个以域名为主得map
+// 日志级别常量
+const (
+	LogLevelDebug = iota
+	LogLevelInfo
+	LogLevelWarn
+	LogLevelError
+)
+
+// 全局日志级别
+var logLevel = LogLevelInfo
+
+// Logger 接口定义
+type Logger interface {
+	Debugf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+}
+
+// 默认使用标准库的日志实现
+type stdLogger struct{}
+
+func (l *stdLogger) Debugf(format string, args ...interface{}) {
+	if logLevel <= LogLevelDebug {
+		log.Printf("[DEBUG] "+format, args...)
+	}
+}
+
+func (l *stdLogger) Infof(format string, args ...interface{}) {
+	if logLevel <= LogLevelInfo {
+		log.Printf("[INFO] "+format, args...)
+	}
+}
+
+func (l *stdLogger) Warnf(format string, args ...interface{}) {
+	if logLevel <= LogLevelWarn {
+		log.Printf("[WARN] "+format, args...)
+	}
+}
+
+func (l *stdLogger) Errorf(format string, args ...interface{}) {
+	if logLevel <= LogLevelError {
+		log.Printf("[ERROR] "+format, args...)
+	}
+}
+
+// 默认日志记录器
+var logger Logger = &stdLogger{}
+
+// 设置日志级别
+func SetLogLevel(level int) {
+	logLevel = level
+}
+
+// 设置自定义日志记录器
+func SetLogger(l Logger) {
+	logger = l
+}
+
+// 创建一个以域名为主的map
 var ConnGroup = sync.Map{}
 
-// 创建一个ws得链接map
+// 创建一个ws的链接map
 var ConnWsGroup = sync.Map{}
 
 type ServerInfo struct {
@@ -75,28 +134,33 @@ type CoreData struct {
 }
 
 var ApiUrl = ""
-
 var CORE_VERSION = "1.0"
-
 var deviceID = "NO_ID"
 
 func Proxy(messageType HpMessage.HpMessage_MessageType, server_ip string, server_port int, username string, password string, domain string, remote_port int, ip string, port int) bool {
 	_, ok := ConnGroup.Load(domain)
 	if ok {
+		logger.Warnf("域名 %s 已经存在，跳过创建", domain)
 		return false
 	}
+
+	logger.Infof("创建新的代理连接: 类型=%s, 服务器=%s:%d, 域名=%s, 远程端口=%d, 目标=%s:%d",
+		messageType, server_ip, server_port, domain, remote_port, ip, port)
+
 	hpClient := tcp.NewHpClient(func(message string) {
-		log.Printf(message)
+		logger.Infof("[%s] %s", domain, message)
 		wsSend(Log{Domain: domain, Msg: message})
 	})
 	hpClient.Connect(messageType, server_ip, server_port, username, password, domain, remote_port, ip, port)
 	go func() {
 		for {
 			if hpClient.IsKill() {
+				logger.Infof("代理连接 %s 已终止", domain)
 				ConnGroup.Delete(domain)
 				return
 			}
 			if !hpClient.GetStatus() {
+				logger.Warnf("代理连接 %s 断开，尝试重连...", domain)
 				hpClient.Connect(messageType, server_ip, server_port, username, password, domain, remote_port, ip, port)
 				wsSend(Log{Domain: domain, Msg: "正在重连"})
 			}
@@ -104,6 +168,7 @@ func Proxy(messageType HpMessage.HpMessage_MessageType, server_ip string, server
 		}
 	}()
 	ConnGroup.Store(domain, hpClient)
+	logger.Infof("代理连接 %s 创建成功", domain)
 	return true
 }
 
@@ -111,13 +176,42 @@ func StartWeb(webPort int, coreVersion string) {
 	CORE_VERSION = coreVersion
 	gin.SetMode(gin.ReleaseMode)
 	gin.DefaultWriter = io.Discard
+
+	logger.Infof("启动Web服务，端口=%d，核心版本=%s", webPort, coreVersion)
+
 	e := gin.Default()
+
+	// 添加Gin日志中间件
+	e.Use(func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		clientIP := c.ClientIP()
+		method := c.Request.Method
+
+		c.Next()
+
+		latency := time.Since(start)
+		status := c.Writer.Status()
+
+		logger.Infof("%s %s %s %d %v | %s",
+			method,
+			path,
+			query,
+			status,
+			latency,
+			clientIP,
+		)
+	})
+
 	e.StaticFS("/static", http.FS(staticFs))
 
 	/**
 	添加穿透
 	*/
 	e.POST("/server/proxy", func(context *gin.Context) {
+		logger.Debugf("收到添加穿透请求: %v", context.Request.PostForm)
+
 		ip := context.PostForm("ip")
 		port := context.PostForm("port")
 		server_info := context.PostForm("server_info")
@@ -167,11 +261,13 @@ func StartWeb(webPort int, coreVersion string) {
 		ato3, _ := strconv.Atoi(port)
 		re := Proxy(messageType, split[0], ato1, username, password, domain, ato2, ip, ato3)
 		if re {
+			logger.Infof("成功添加穿透: domain=%s, server=%s, target=%s:%s", domain, server_info, ip, port)
 			context.JSON(http.StatusOK, &Res{
 				Code: 200,
 				Msg:  "添加成功",
 			})
 		} else {
+			logger.Warnf("添加穿透失败: domain=%s 可能已被使用", domain)
 			context.JSON(http.StatusOK, &Res{
 				Code: -1,
 				Msg:  "添加失败！检查域名是否已经被使用。",
@@ -183,6 +279,7 @@ func StartWeb(webPort int, coreVersion string) {
 	当前穿透列表
 	*/
 	e.GET("/server/info", func(context *gin.Context) {
+		logger.Debugf("获取当前穿透列表请求")
 		ret := make([]*ServerInfo, 0)
 		ConnGroup.Range(func(key, value interface{}) bool {
 			client := value.(*tcp.HpClient)
@@ -202,16 +299,20 @@ func StartWeb(webPort int, coreVersion string) {
 	*/
 	e.GET("/server/stop", func(context *gin.Context) {
 		domain := context.Query("domain")
+		logger.Infof("收到停止穿透请求: domain=%s", domain)
+
 		load, ok := ConnGroup.Load(domain)
 		if ok {
 			client := load.(*tcp.HpClient)
 			client.Kill()
 			ConnGroup.Delete(domain)
+			logger.Infof("成功停止穿透: domain=%s", domain)
 			context.JSON(http.StatusOK, &Res{
 				Code: 200,
 				Msg:  "成功",
 			})
 		} else {
+			logger.Warnf("停止穿透失败: domain=%s 不存在", domain)
 			context.JSON(http.StatusOK, &Res{
 				Code: 200,
 				Msg:  "失败",
@@ -223,18 +324,20 @@ func StartWeb(webPort int, coreVersion string) {
 	内核版本
 	*/
 	e.GET("/core/version", func(context *gin.Context) {
+		logger.Debugf("获取内核版本请求")
 		resp, err := http.Get(ApiUrl + "/app/getCoreVersion")
 		if err != nil {
-			log.Println(err)
+			logger.Errorf("获取内核版本失败: %v", err)
 		}
 		defer func(Body io.ReadCloser) {
 			err := Body.Close()
 			if err != nil {
-				log.Println(err)
+				logger.Errorf("关闭响应体失败: %v", err)
 			}
 		}(resp.Body)
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
+			logger.Errorf("读取内核版本响应失败: %v", err)
 			context.JSON(http.StatusOK, &Res{
 				Code: -1,
 				Msg:  "检查更新失败",
@@ -244,6 +347,7 @@ func StartWeb(webPort int, coreVersion string) {
 		data := &CoreData{}
 		err = json.Unmarshal(body, data)
 		if err != nil || data.Data == nil {
+			logger.Errorf("解析内核版本数据失败: %v", err)
 			context.JSON(http.StatusOK, &Res{
 				Code: -1,
 				Msg:  "检查更新失败",
@@ -252,11 +356,13 @@ func StartWeb(webPort int, coreVersion string) {
 		}
 		//如果相等
 		if strings.Compare(data.Data.VersionCode, CORE_VERSION) == 0 {
+			logger.Infof("内核版本检查: 当前版本 %s 已是最新", CORE_VERSION)
 			context.JSON(http.StatusOK, &Res{
 				Code: 200,
 				Msg:  "<font color='green'>不需要更新</font><br>当前版本:" + CORE_VERSION + "<br>你的Proxy内核已经是最新版",
 			})
 		} else {
+			logger.Infof("内核版本检查: 当前版本 %s, 最新版本 %s", CORE_VERSION, data.Data.VersionCode)
 			context.JSON(http.StatusOK, &Res{
 				Code: 200,
 				Msg:  "<font color='red'>需要更新</font><br>当前版本:" + CORE_VERSION + "<br>最新版本:" + data.Data.VersionCode + "<br>更新时间:" + data.Data.CreateTime + "<br>更新内容:" + data.Data.UpdateContent,
@@ -268,6 +374,7 @@ func StartWeb(webPort int, coreVersion string) {
 	查询设备ID
 	*/
 	e.GET("/device/info", func(context *gin.Context) {
+		logger.Debugf("查询设备ID请求")
 		context.JSON(http.StatusOK, deviceID)
 	})
 
@@ -275,14 +382,17 @@ func StartWeb(webPort int, coreVersion string) {
 	网页端请求前缀配置
 	*/
 	e.GET("/api.js", func(context *gin.Context) {
+		logger.Debugf("获取API配置请求")
 		context.String(http.StatusOK, "var apiAddress = \"/hp\"")
 	})
 	/**
 	反向代理，前缀解析然后反向代理
 	*/
 	e.Any("/hp/*url", func(c *gin.Context) {
+		logger.Debugf("反向代理请求: %s", c.Param("url"))
 		remote, err := url.Parse(ApiUrl)
 		if err != nil {
+			logger.Errorf("解析API URL失败: %v", err)
 			panic(err)
 		}
 		proxy := httputil.NewSingleHostReverseProxy(remote)
@@ -297,6 +407,7 @@ func StartWeb(webPort int, coreVersion string) {
 	})
 
 	e.GET("/", func(context *gin.Context) {
+		logger.Debugf("根路径请求，重定向到登录页")
 		context.Redirect(http.StatusMovedPermanently, "/static/login.html")
 	})
 
@@ -304,7 +415,11 @@ func StartWeb(webPort int, coreVersion string) {
 	if webPort <= 0 {
 		webPort = 10240
 	}
-	e.Run(":" + strconv.Itoa(webPort))
+	logger.Infof("Web服务监听端口: %d", webPort)
+	err := e.Run(":" + strconv.Itoa(webPort))
+	if err != nil {
+		logger.Errorf("启动Web服务失败: %v", err)
+	}
 }
 
 var upGrader = websocket.Upgrader{
@@ -315,52 +430,55 @@ var upGrader = websocket.Upgrader{
 
 func wsSend(msg Log) {
 	defer func() {
-		recover()
+		if r := recover(); r != nil {
+			logger.Errorf("wsSend panic: %v", r)
+		}
 	}()
 	ConnWsGroup.Range(func(key, value interface{}) bool {
 		WS := key.(*websocket.Conn)
 		err := WS.WriteJSON(msg)
 		if err != nil {
+			logger.Errorf("发送WebSocket消息失败: %v", err)
 			return false
 		}
 		return true
 	})
 }
 
-// InitCloudDevice /**
-func InitCloudDevice(apiAddress string, deviceId string) {
+func InitCloudDevice(apiAddress string, deviceId string, level int) {
+	SetLogLevel(level)
 	ApiUrl = apiAddress
 	defer func() {
-		err := recover()
-		if err != nil {
-			log.Println("云端资源读取失败", err)
+		if err := recover(); err != nil {
+			logger.Errorf("云端资源读取失败: %v", err)
 		}
 	}()
 	if deviceId == "NO_ID" {
-		log.Println("未获取道设备ID，不能加载云端资源")
+		logger.Warnf("未获取到设备ID，不能加载云端资源")
 		return
 	} else {
 		//校验设备ID
 		matched, _ := regexp.MatchString("^[0-9a-zA-Z]+$", deviceId)
 		if !matched || !(len(deviceId) >= 10 && len(deviceId) <= 36) {
-			log.Println("设备ID只能是数字和字母组成同时大于10-36位")
+			logger.Errorf("设备ID无效: %s (只能是数字和字母组成同时大于10-36位)", deviceId)
 			return
 		}
 	}
 	deviceID = deviceId
+	logger.Infof("初始化云设备连接，API地址=%s，设备ID=%s", apiAddress, deviceId)
 	resp, err := http.Get(ApiUrl + "/config/listDevice?deviceId=" + deviceId)
 	if err != nil {
-		log.Println(err)
+		logger.Errorf("获取设备配置失败: %v", err)
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			log.Println(err)
+			logger.Errorf("关闭响应体失败: %v", err)
 		}
 	}(resp.Body)
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("获取云端资源失败")
+		logger.Errorf("读取设备配置响应失败: %v", err)
 	}
 	data := &DeviceData{}
 	err = json.Unmarshal(body, data)
@@ -376,7 +494,7 @@ func InitCloudDevice(apiAddress string, deviceId string) {
 			} else if info.Type == "TCP_UDP" {
 				hpType = HpMessage.HpMessage_TCP_UDP
 			} else {
-				log.Println("穿透类型未知：" + info.Type)
+				logger.Errorf("穿透类型未知: %s", info.Type)
 				return
 			}
 
@@ -390,12 +508,13 @@ func InitCloudDevice(apiAddress string, deviceId string) {
 			userPort, _ := strconv.Atoi(split2[1])
 			re := Proxy(hpType, serverIp, serverPort, info.Username, info.Password, info.Domain, port, userIp, userPort)
 			if re {
-				log.Println("内网服务：" + info.UserHost + " 启动成功")
+				logger.Infof("内网服务启动成功: %s -> %s", info.UserHost, info.Domain)
 			} else {
-				log.Println("内网服务：" + info.UserHost + " 启动失败")
+				logger.Errorf("内网服务启动失败: %s -> %s", info.UserHost, info.Domain)
 			}
 		}
-
+	} else {
+		logger.Errorf("解析设备配置失败: %v", err)
 	}
 }
 
@@ -403,17 +522,21 @@ func ws(c *gin.Context) {
 	//升级get请求为webSocket协议
 	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
+		logger.Errorf("WebSocket升级失败: %v", err)
 		return
 	}
+	logger.Infof("新的WebSocket连接建立")
 	ConnWsGroup.Store(ws, nil)
 	defer func() {
 		ConnWsGroup.Delete(ws)
 		ws.Close()
+		logger.Infof("WebSocket连接关闭")
 	}()
 	for {
 		//读取ws中的数据
 		mt, message, err := ws.ReadMessage()
 		if err != nil {
+			logger.Errorf("读取WebSocket消息失败: %v", err)
 			break
 		}
 		if string(message) == "ping" {
@@ -422,6 +545,7 @@ func ws(c *gin.Context) {
 		//写入ws数据
 		err = ws.WriteMessage(mt, message)
 		if err != nil {
+			logger.Errorf("写入WebSocket消息失败: %v", err)
 			break
 		}
 	}
