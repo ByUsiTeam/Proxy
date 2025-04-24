a87dc96f5a198a77478237d7aa214685f58e9105
北啊呢 <177828525@qq.com>
2025-04-24 11:57:19 +0000

    update proxy-client-golang/main.go.
    Signed-off-by: 北啊呢 <177828525@qq.com>

--- proxy-client-golang/main.go
+++ proxy-client-golang/main.go
@@ -9,15 +9,21 @@ import (
 
 func main() {
 	var deviceId string
-	//命令行参数模式
+	var logLevel int
+
+	// 命令行参数模式
 	flag.StringVar(&deviceId, "deviceId", "NO_ID", "设备ID")
+	flag.IntVar(&logLevel, "logLevel", web.LogLevelInfo, "日志级别(0=Debug,1=Info,2=Warn,3=Error)")
 	flag.Parse()
-	//默认命令行参数大于环境变量参数
+
+	// 默认命令行参数大于环境变量参数
 	e := os.Getenv("deviceId")
 	if deviceId == "NO_ID" && e != "" {
 		deviceId = e
 	}
-	web.InitCloudDevice("http://proxy.byusi.cn:9090", deviceId)
+
+	log.Printf("启动参数: deviceId=%s, logLevel=%d", deviceId, logLevel)
+	web.InitCloudDevice("http://proxy.byusi.cn:9090", deviceId, logLevel)
 	log.Printf("请访问 http://127.0.0.1:10240/ 进行穿透配置")
-	web.StartWeb(0, "15.0")
+	web.StartWeb(0, "15.4")
 }
