56fc42bdbc4407362c449b06ecd5e64d28a613d3
北啊呢 <177828525@qq.com>
2026-02-17 17:58:37 +0800

    解决软件端编译问题（终于有正经的安卓软件端可以用啦）

--- proxy-android-client/app/src/main/java/miao/byusi/proxy_client/MainActivity.java
+++ proxy-android-client/app/src/main/java/miao/byusi/proxy_client/MainActivity.java
@@ -180,11 +180,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_about:
-                createAboutAlert().show();
-                break;
+        int id =item.getItemId();
+        if (id == R.id.action_about) {
+            createAboutAlert().show();
         }
+        
 
         return super.onOptionsItemSelected(item);
     }
